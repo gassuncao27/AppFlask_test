db2de3a5676f527fd19cde73163c2c99b66a0c44
gassuncao27 <gassuncao27@gmail.com>
2023-07-17 09:12:13 -0300

    Avanços no curso - secao 2 / part 10

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.debug = True
+# app.run()
 
 # set FLASK_APP = app.py
 # set FLASK_ENV=development
 
-@app.route('/') # rota principal
-def principal():
-    frutas = ["TENDA", "MRV", "ECORODOVIAS", "CSN", "CURY"]    
+frutas =[]
 
+@app.route('/', methods=["GET", "POST"]) # rota principal
+def principal():
+    # frutas = ["Tenda", "Mrv", "Ecorodovias", "Csn", "Cury"]    
+    if request.method == "POST":
+        if request.form.get("fruta"):
+            frutas.append(request.form.get("fruta"))
     return render_template("index.html", frutas=frutas) # ao inves desse retorno vamos retornar uma pagina html
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
+    notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno":7.0, "Sicrano":8.5, "Rodrigo":9.5}
+    return render_template("sobre.html", notas=notas)
