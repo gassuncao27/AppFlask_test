eae119d892efb75cc9ca5594d75f93db18b13491
gassuncao27 <gassuncao27@gmail.com>
2023-07-17 13:54:55 -0300

    Curso secao 3, capitulo 13

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.debug = True
-# app.run()
-
 # set FLASK_APP = app.py
 # set FLASK_ENV=development
 
 frutas =[]
+registros = []
 
 @app.route('/', methods=["GET", "POST"]) # rota principal
 def principal():
@@ -17,7 +15,15 @@ def principal():
             frutas.append(request.form.get("fruta"))
     return render_template("index.html", frutas=frutas) # ao inves desse retorno vamos retornar uma pagina html
 
-@app.route('/sobre')
+@app.route('/sobre', methods=['GET', 'POST'])
 def sobre():
-    notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno":7.0, "Sicrano":8.5, "Rodrigo":9.5}
-    return render_template("sobre.html", notas=notas)
+    # notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno":7.0, "Sicrano":8.5, "Rodrigo":9.5}
+    if request.method == 'POST':
+        if request.form.get('aluno') and request.form.get('nota'):
+            registros.append({"aluno": request.form.get("aluno"), 'nota': request.form.get("nota")})
+    
+    return render_template("sobre.html", registros=registros)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
